9a24beb27554f9b81e16ccd4a83789a1cd5e4a58
jd-create <jenniferdemeijer@t-online.de>
2021-04-06 17:10:47 +0200

    first steps in making 'geld storten programma'

--- bank.js
+++ bank.js
@@ -31,17 +31,17 @@
 // x Saldo uit object halen
 // x Saldo weergeven
 
-const accounts = [
-  { id: 1, name: "Tim", iban: "INGB110000999911", pin: "1234", saldo: 250 }, // 0
-  { id: 2, name: "Nina", iban: "INGB110000999922", pin: "9876", saldo: 3300 }, // 1
-  { id: 3, name: "Karel", iban: "INGB110000999987", pin: "0001", saldo: 10 }, // 2
-  { id: 4, name: "Sjaan", iban: "INGB110000999989", pin: "0222", saldo: 10000 }, // 2
-];
-
-// SCENARIO 1 (happy path! 😃)
-
-const ibanInput = "INGB110000999922";
-const pinInput = "9876";
+// const accounts = [
+//   { id: 1, name: "Tim", iban: "INGB110000999911", pin: "1234", saldo: 250 }, // 0
+//   { id: 2, name: "Nina", iban: "INGB110000999922", pin: "9876", saldo: 3300 }, // 1
+//   { id: 3, name: "Karel", iban: "INGB110000999987", pin: "0001", saldo: 10 }, // 2
+//   { id: 4, name: "Sjaan", iban: "INGB110000999989", pin: "0222", saldo: 10000 }, // 2
+// ];
+//
+// // SCENARIO 1 (happy path! 😃)
+//
+// const ibanInput = "INGB110000999922";
+// const pinInput = "9876";
 
 // DESIRED OUTPUT
 // Uw saldo bedraagt: €3300
@@ -56,6 +56,60 @@ const pinInput = "9876";
 // DESIRED OUTPUT
 // Uw pincode is niet correct, probeer het nog eens
 
+// let account;
+//
+// for (const element of accounts) {
+//   if (element.iban === ibanInput) {
+//     account = element;
+//   }
+// }
+//
+// if (account.pin === pinInput) {
+//   const saldo = account.saldo;
+//   console.log("Uw saldo bedraagt: €" + saldo);
+// } else {
+//   console.log("Uw pincode is niet correct, probeer het nog eens");
+// }
+
+// Geld storten
+//Geld storten:
+
+//    Rekening -> Object
+//     id: number
+//     naam: string
+//     nummer: string
+//     pin: string
+//     saldo: number
+// Rekeningen -> Array<Rekening>
+//
+// Input: Rekeningen, iban, pin, bedrag dat gestort wordt
+// Output: Saldo opgehoogd met bedrag dat gestort werd + nieuw verhoogd saldo
+
+// Stappen
+// [x] variabelen aanmaken
+//  [X] Rekeningen aanmaken (dummy data)
+//  [X] iban nodig (dummy input)
+// - zoeken naar rekening met de iban uit de input
+// x checken: is de pin correct?
+// x gestort bedrag uit de input verhogen bij het saldo uit het object
+// x Verhoogd Saldo weergeven
+
+const accounts = [
+  { id: 1, name: "Tim", iban: "INGB110000999911", pin: "1234", saldo: 250 }, // 0
+  { id: 2, name: "Nina", iban: "INGB110000999922", pin: "9876", saldo: 3300 }, // 1
+  { id: 3, name: "Karel", iban: "INGB110000999987", pin: "0001", saldo: 10 }, // 2
+  { id: 4, name: "Sjaan", iban: "INGB110000999989", pin: "0222", saldo: 10000 }, // 2
+];
+
+const ibanInput = "INGB110000999911";
+const pinInput = "1234";
+const depositSum = 25;
+let saldoVoorStorten = 0;
+const saldoNaStorten = depositSum + saldoVoorStorten;
+
+// SCENARIO 1 (happy path! 😃)
+// Uw saldo bedraagt: €3300 + €INPUT = €OUTPUT
+
 let account;
 
 for (const element of accounts) {
@@ -65,11 +119,12 @@ for (const element of accounts) {
 }
 
 if (account.pin === pinInput) {
-  const saldo = account.saldo;
-  console.log("Uw saldo bedraagt: €" + saldo);
+  saldoVoorStorten = account.saldo;
+  console.log("Uw hebt uw saldo verhoogt met €" + depositSum +  ", uw saldo bedraagt nu: €" + saldoNaStorten);
 } else {
   console.log("Uw pincode is niet correct, probeer het nog eens");
 }
 
-// Geld storten
+
+
 // Geld opnemen
