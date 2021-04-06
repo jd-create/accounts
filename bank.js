f4928c2a6bf968a40f3f07ad88cd048397df48eb
Reinoptland <reinoptland@gmail.com>
2021-04-06 14:47:22 +0200

    Make plan

---
+++ bank.js
@@ -0,0 +1,26 @@
+// Functie eisen -> Data modelleren -> Stappenplan
+
+// Saldo opvragen
+// Rekening -> Object
+//     id: number
+//     naam: string
+//     nummer: string
+//     pin: string
+//     saldo: number
+// Rekeningen -> Array<Rekening>
+//
+// Input: Rekeningen, id, pin
+// Output: Saldo
+
+// Stappen
+// - variabelen aanmaken
+//  - Rekeningen aanmaken (dummy data)
+//  - id nodig (dummy input)
+//  - pin nodig (dummy input)
+// - zoeken naar rekening met de id uit de input
+// - checken: is de pin correct?
+// - Saldo uit object halen
+// - Saldo weergeven
+
+// Geld storten
+// Geld opnemen
